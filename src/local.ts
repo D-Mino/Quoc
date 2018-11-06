217188982af98e1fafa4dc37118d1c4243a5ef6c
Tran Xuan Quoc <tranxuanquoc96@gmail.com>
2018-11-06 14:31:59 +0700

    init

--- src/local.ts
+++ src/local.ts
@@ -1,16 +1,16 @@
 const hostname = window.location.hostname;
 
-const local = 'http://localhost/stam/logger-api';
-const test = 'http://test.cyberair.co/logger-api';
-const live = 'https://stambridgesecurity.co.uk/logger-api';
+const local = 'http://localhost/stam/vnc-api';
+const test = 'http://localhost/stam/vnc-api';
+const live = 'http://localhost/stam/vnc-api';
 
 const api = url => `${url}/`;
 
 const settings: any = {};
 
 settings.local = hostname === 'localhost';
-settings.test = hostname === 'test.cyberair.co';
-settings.live = hostname === 'stambridgesecurity.co.uk';
+settings.test = hostname === 'localhost';
+settings.live = hostname === 'localhost';
 
 settings.api = {
   local: api(local),
